d1ee4ae904da1ec82e3a3de6ecb0c5b3476142e8
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-07-27 13:27:10 -0400

    Add support for new features

---
+++ app/chat-ui/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  isProduction: false,
+  serverUrl: 'http://127.0.0.1:8080',
+  chatEndpoint: '/chat'
+};
---
+++ app/chat-ui/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  isProduction: true,
+  serverUrl: 'http://127.0.0.1:8080',
+  chatEndpoint: '/chat'
+};
